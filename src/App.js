aeee2cc6355fcb2437a2382114c95abd9c88d377
joy1302 <69805179+joy1302@users.noreply.github.com>
2023-04-16 17:11:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainContent from './components/MainContent';
-import AboutUs from './Components/AboutUs'
+import AboutUs from './components/AboutUs'
 
 
 function App() {
